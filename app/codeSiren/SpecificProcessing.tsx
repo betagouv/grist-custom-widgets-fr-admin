c893969c3d0d388e49e6957ed016d0a68161933a
CamilleLegeron <camille@telescoop.fr>
2023-12-21 11:08:56 +0100

    mutualisation specificProcessing

--- app/codeSiren/SpecificProcessing.tsx
+++ app/codeSiren/SpecificProcessing.tsx
@@ -4,12 +4,11 @@ import { FC } from "react";
 import { RowRecord } from "grist/GristData";
 import { WidgetColumnMap } from "grist/CustomSectionAPI";
 import { COLUMN_MAPPING_NAMES } from "./constants";
-import Image from "next/image";
-import doneSvg from "../../public/done.svg";
 import { NormalizedSirenResult } from "./types";
 import GenericChoiceBanner from "../../components/GenericChoiceBanner";
 import { DirtyRecord, NoResultRecord } from "../../lib/util/types";
 import RecordName from "../../components/RecordName";
+import GenericSpecificProcessing from "../../components/GenericSpecificProcessing";
 
 export const SpecificProcessing: FC<{
   mappings: WidgetColumnMap | null;
@@ -31,7 +30,7 @@ export const SpecificProcessing: FC<{
   recordResearch,
   goBackToMenu,
 }) => {
-  const recordName = (
+  const recordNameNode = (
     <RecordName
       record={record}
       columnName={mappings && mappings[COLUMN_MAPPING_NAMES.NAME.name]}
@@ -41,87 +40,48 @@ export const SpecificProcessing: FC<{
   const isResultFind = () => {
     if (record && mappings) {
       const columnName = mappings[COLUMN_MAPPING_NAMES.SIREN.name];
-      return typeof columnName === "string" && record[columnName] && true;
+      if (typeof columnName === "string" && record[columnName]) {
+        return true;
+      }
     }
     return false;
   };
 
-  const selectOtherLine = (
-    <>
-      <p>Sélectionner une autre ligne à traiter spécifiquement</p>
-      <p>ou</p>
-    </>
+  const recordFindNode = (
+    <div>Le code SIREN de {recordNameNode} a bien été rempli.</div>
   );
 
-  const actionsButton = (isFirstResearch: boolean) => {
-    return (
-      <>
-        {record && (
-          <button className="primary" onClick={recordResearch}>
-            {isFirstResearch ? "Recherche spécifique" : "Réitérer la recherche"}
-          </button>
-        )}
-        <button className="secondary" onClick={goBackToMenu}>
-          Retour à l'accueil
-        </button>
-      </>
-    );
-  };
+  const choiceBannerNode = record && dirtyData && (
+    <GenericChoiceBanner<NormalizedSirenResult>
+      dirtyData={dirtyData}
+      passDataFromDirtyToClean={passDataFromDirtyToClean}
+      option={{
+        choiceValueKey: "siren",
+        withChoiceTagLegend: true,
+        choiceTagLegend: "Code SIREN",
+        choiceTagKey: "siren",
+      }}
+      itemDisplay={(item: NormalizedSirenResult) => {
+        return (
+          <div>
+            <b>{item.label}</b>
+            {item.code_commune && ` - ${item.code_commune}`}
+          </div>
+        );
+      }}
+    />
+  );
 
-  return isResultFind() ? (
-    <div className="centered-column">
-      <h2>Traitement spécifique terminé</h2>
-      <Image
-        priority
-        src={doneSvg}
-        style={{ marginBottom: "1rem" }}
-        alt="traitement spécifique terminé"
-      />
-      <div>Le code SIREN de {recordName} a bien été rempli.</div>
-      <div style={{ marginTop: "4rem" }}>
-        {selectOtherLine}
-        {actionsButton(false)}
-      </div>
-    </div>
-  ) : (
-    <div className="centered-column">
-      <h2>Traitement spécifique</h2>
-      <div>Collectivité sélectionnée : {recordName}</div>
-
-      {record && dirtyData && (
-        <GenericChoiceBanner<NormalizedSirenResult>
-          dirtyData={dirtyData}
-          passDataFromDirtyToClean={passDataFromDirtyToClean}
-          option={{
-            choiceValueKey: "siren",
-            withChoiceTagLegend: true,
-            choiceTagLegend: "Code SIREN",
-            choiceTagKey: "siren",
-          }}
-          itemDisplay={(item: NormalizedSirenResult) => {
-            return (
-              <div>
-                <b>{item.label}</b>
-                {item.code_commune && ` - ${item.code_commune}`}
-              </div>
-            );
-          }}
-        />
-      )}
-      {record && noResultData && (
-        <div className="py-2">
-          <span className="semi-bold">{noResultData.noResultMessage}</span>
-        </div>
-      )}
-      <div style={{ marginTop: "4rem" }}>
-        {record && noResultData ? (
-          <>
-            {selectOtherLine} {actionsButton(false)}
-          </>
-        ) : (
-          actionsButton(true)
-        )}
-      </div>
-    </div>
+  return (
+    <GenericSpecificProcessing<NormalizedSirenResult>
+      record={record}
+      recordNameNode={recordNameNode}
+      noResultData={noResultData}
+      recordResearch={recordResearch}
+      goBackToMenu={goBackToMenu}
+      isResultFind={isResultFind}
+      recordFindNode={recordFindNode}
+      choiceBannerNode={choiceBannerNode}
+    />
   );
 };
