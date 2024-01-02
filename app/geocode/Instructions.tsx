35ec600abf798b208300316bf95d51214660244d
CamilleLegeron <camille@telescoop.fr>
2024-01-02 15:21:03 +0100

    mutualisation: instruction on accordion + siren instruction corrections

--- app/geocode/Instructions.tsx
+++ app/geocode/Instructions.tsx
@@ -1,50 +1,39 @@
 "use client";
 
-import { useState } from "react";
+import { Accordion } from "../../components/Accordion";
 
 export const Instructions = () => {
-  const [isInfoActive, setIsInfoActive] = useState(false);
-  return (
-    <div>
-      <div
-        className="accordion-title"
-        onClick={() => setIsInfoActive(!isInfoActive)}
-      >
-        <div>Afficher les instructions</div>
-        <div>{isInfoActive ? "-" : "+"}</div>
-      </div>
-      {isInfoActive && (
-        <p className="accordion-body">
-          Cette Vue permet d'indiquer le Geocodage (longitude et latitude)
-          correspondant à chaque ligne.
-          <br />
-          Fonctionnement :
-          <ul>
-            <li>
-              Indiquer la colonne respondant à l'adresse ainsi que les colonnes
-              de longitude et latitude (colonne de type Numérique). Il est
-              possible d'ajouter la colonne Adresse Normalisée pour indiquer le
-              nom officiel du géocodage trouvé.
-            </li>
-            <li>
-              Faire une recherche globale afin de faire une première passe sur
-              toutes vos lignes.
-            </li>
-            <li>
-              Désanbiguer ligne par ligne. La raison de pourquoi la Vue n'a pas
-              réussi à remplir le géocodage automatiquement vous sera indiqué.
-              Attention: si la ligne selectionnée reste bloquée sur la première
-              de votre table c'est que le lien entre la Vue et la table est
-              manquant. Vous devez indiquer dans les données sources de la Vue
-              le "Selectionner par".
-            </li>
-            <li>
-              Une fois le nettoyage de la donnée effectuée vous pouvez supprimer
-              cette vue.
-            </li>
-          </ul>
-        </p>
-      )}
-    </div>
+  const instructions = (
+    <>
+      Cette Vue permet d'indiquer le Geocodage (longitude et latitude)
+      correspondant à chaque ligne.
+      <br />
+      Fonctionnement :
+      <ul>
+        <li>
+          Indiquer la colonne respondant à l'adresse ainsi que les colonnes de
+          longitude et latitude (colonne de type Numérique). Il est possible
+          d'ajouter la colonne Adresse Normalisée pour indiquer le nom officiel
+          du géocodage trouvé.
+        </li>
+        <li>
+          Faire une recherche globale afin de faire une première passe sur
+          toutes vos lignes.
+        </li>
+        <li>
+          Désanbiguer ligne par ligne. La raison de pourquoi la Vue n'a pas
+          réussi à remplir le géocodage automatiquement vous sera indiqué.
+          Attention: si la ligne selectionnée reste bloquée sur la première de
+          votre table c'est que le lien entre la Vue et la table est manquant.
+          Vous devez indiquer dans les données sources de la Vue le
+          "Selectionner par".
+        </li>
+        <li>
+          Une fois le nettoyage de la donnée effectuée vous pouvez supprimer
+          cette vue.
+        </li>
+      </ul>
+    </>
   );
+  return <Accordion label="Afficher les instructions" body={instructions} />;
 };
---
+++ components/Accordion.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { FC, ReactNode, useState } from "react";
+import "./accordion.css";
+
+export const Accordion: FC<{
+  label: string;
+  body: ReactNode;
+}> = ({ label, body }) => {
+  const [isAccordionActive, setIsAccordionActive] = useState(false);
+  return (
+    <div>
+      <div
+        className="accordion-title"
+        onClick={() => setIsAccordionActive(!isAccordionActive)}
+      >
+        <div>{label}</div>
+        <div>{isAccordionActive ? "-" : "+"}</div>
+      </div>
+      {isAccordionActive && <p className="accordion-body">{body}</p>}
+    </div>
+  );
+};
