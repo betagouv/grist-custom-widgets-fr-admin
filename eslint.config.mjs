f34c746ff3482b657ae45005de8499b92f4df33d
fflorent <florent.git@zeteo.me>
2025-01-14 16:31:07 +0100

    Eslint: migrate the configuration file
    See: https://eslint.org/docs/latest/use/configure/migration-guide

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/node_modules", "**/.next"],
+}, ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslintEslintPlugin,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    rules: {
+        curly: ["error", "all"],
+        "react/no-unescaped-entities": "off",
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            varsIgnorePattern: "^_",
+            argsIgnorePattern: "^_",
+            ignoreRestSiblings: true,
+        }],
+    },
+}];
